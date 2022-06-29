980d53d3514f0fd8bf9228d93a6bab2061360418
MJWORKS <mjworks2020@outlook.com>
2022-06-29 10:52:07 -0300

    Added new changes

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+@Injectable()
 export class AccountService {
   accounts = [
     {
@@ -14,11 +17,15 @@ export class AccountService {
     },
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string): void {
     this.accounts.push({ name: name, status: status });
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string): void {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
